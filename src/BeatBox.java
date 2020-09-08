15e48155196f0fd783f788f400ba9c6ba8c62f92
Mitya <techwipes@gmail.com>
2020-09-08 15:33:41 +0500

    add buildTrackAndStart method

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -93,4 +93,39 @@ public class BeatBox {
         }
     }
 
+    public void buildTrackAndStart() {
+        int[] trackList = null;
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+            int key = instruments[i];
+
+            for (int j = 0; j < 16; j++) {
+
+                JCheckBox jc = (JCheckBox) checkboxList.get(j + (16 * i));
+                if (jc.isSelected()) {
+                    trackList[j] = key;
+                } else {
+                    trackList[j] = 0;
+                }
+            }
+
+            makeTracks(trackList);
+            track.add(makeEvent(176, 1, 127, 0, 16));
+        }
+        track.add(makeEvent(192, , 9, 1, 0, 15));
+        try {
+
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
